9314c4581143552d1d90bf1ef842dbaceea3a679
Marquis Sampson <marquis4484@gmail.com>
2023-09-19 17:30:29 -0400

    js changes

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,70 +2,31 @@
 import '../toggleSidebar.js';
 import '../cart/toggleCart.js';
 import '../cart/setupCart.js';
-// specific
-import { addToCart } from '../cart/setupCart.js';
-import { singleProductUrl, getElement, formatPrice } from '../utils.js';
 
-// selections
-const loading = getElement('.page-loading');
-const centerDOM = getElement('.single-product-center');
-const pageTitleDOM = getElement('.page-hero-title');
-const imgDOM = getElement('.single-product-img');
-const titleDOM = getElement('.single-product-title');
-const companyDOM = getElement('.single-product-company');
-const priceDOM = getElement('.single-product-price');
-const colorsDOM = getElement('.single-product-colors');
-const descDOM = getElement('.single-product-desc');
-const cartBtn = getElement('.addToCartBtn');
+//  filter imports
+import setupSearch from '../filters/search.js';
+import setupCompanies from '../filters/companies.js';
+import setupPrice from '../filters/price.js';
 
-// cart product
-let productID;
+// specific imports
+import { store, setupStore } from '../store.js';
+import display from '../displayProducts.js';
+import { getElement } from '../utils.js';
+// import fetch products
+import fetchProducts from '../fetchProducts.js';
 
-// show product when page loads
-window.addEventListener('DOMContentLoaded', async function () {
-  const urlID = window.location.search;
-
-  try {
-    const response = await fetch(`${singleProductUrl}${urlID}`);
-    if (response.status >= 200 && response.status <= 299) {
-      const product = await response.json();
-      // grab data
-      const { id, fields } = product;
-      productID = id;
-
-      const { name, company, price, colors, description } = fields;
-      const image = fields.image[0].thumbnails.large.url;
-      // set values
-
-      document.title = `${name.toUpperCase()} | Comfy`;
-      pageTitleDOM.textContent = `Home / ${name}`;
-      imgDOM.src = image;
-      titleDOM.textContent = name;
-      companyDOM.textContent = `by ${company}`;
-      priceDOM.textContent = formatPrice(price);
-      descDOM.textContent = description;
-      colors.forEach((color) => {
-        const span = document.createElement('span');
-        span.classList.add('product-color');
-        span.style.backgroundColor = `${color}`;
-        colorsDOM.appendChild(span);
-      });
-    } else {
-      console.log(response.status, response.statusText);
-      centerDOM.innerHTML = `
-    <div>
-    <h3 class="error">sorry, something went wrong</h3>
-    <a href="index.html" class="btn">back home</a>
-    </div> 
-     `;
-    }
-  } catch (error) {
-    console.log(error);
+const init = async () => {
+  const loading = getElement('.page-loading');
+  if (store.length < 1) {
+    const products = await fetchProducts();
+    setupStore(products);
   }
+  display(store, getElement('.products-container'));
 
+  setupSearch(store);
+  setupCompanies(store);
+  setupPrice(store);
   loading.style.display = 'none';
-});
+};
 
-cartBtn.addEventListener('click', function () {
-  addToCart(productID);
-});
+init();
