72daa4ca60cdd02761a3cbe9d1718e3f7680094c
Marquis Sampson <marquis4484@gmail.com>
2024-09-27 14:25:35 -0400

    toggle button

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,12 +1,14 @@
 import { getElement } from "./utils.js";
 
-const toggleNav = getElement(".toggle-nav");
-const sidebarOverlay = getElement(".sidebar-overlay");
-const closeBtn = getElement(".sidebar-close");
+const toggleNav = document.getElement(".toggle-nav");
+const sidebarOverlay = document.getElement(".sidebar-overlay");
+const closeBtn = document.getElement(".sidebar-close");
 
 toggleNav.addEventListener("click", () => {
+  console.log("shift");
   sidebarOverlay.classList.add("show");
 });
 closeBtn.addEventListener("click", () => {
+  console.log("shift8");
   sidebarOverlay.classList.remove("show");
 });
