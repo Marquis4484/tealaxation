efe3982ba9193c1db16e8f7cd35446d6bd6a299a
Marquis Sampson <marquis4484@gmail.com>
2024-11-19 14:16:03 -0500

    index update

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const init = async () => {
   if (products) {
     // add products to the store
     setupStore(products);
-    const featured = store.filter((product) => product.featured === true);
-    display(featured, getElement(".featured-center"));
   }
 };
 
--- src/store.js
+++ src/store.js
@@ -4,10 +4,10 @@ const setupStore = (products) => {
   store = products.map((product) => {
     const {
       id,
-      fields: { featured, name, price, company, colors, image: img },
+      fields: {  name, price, company, colors, image: img },
     } = product;
     const image = img[0].thumbnails.large.url;
-    return { id, featured, name, price, company, colors, image };
+    return { id, name, price, company, colors, image };
   });
   setStorageItem("store", store);
 };
