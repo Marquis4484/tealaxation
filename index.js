6faf98e7745fbf0110a986b4bd6d512ec1a3f125
Marquis Sampson <marquis4484@gmail.com>
2024-11-27 20:11:50 -0500

    product update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import "./src/cart/setupCart.js";
 // specific imports
 import fetchProducts from "./src/fetchProducts.js";
 import { setupStore, store } from "./src/store.js";
-import display from "./src/displayProducts.js";
+// import display from "./src/displayProducts.js";
 import { getElement } from "./src/utils.js";
 
 const init = async () => {
